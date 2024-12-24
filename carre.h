806cf4d628bb49270d8a4238ebecbecdd2c3a34d
BON-CIEL <maximebonnevay.branly@gmail.com>
2024-12-24 12:17:16 +0100

    Commit #2

--- carre.h
+++ carre.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include <iostream>
+
+class CCarre {
+private:
+    int sx;  // Coordonnée x du sommet supérieur gauche
+    int sy;  // Coordonnée y du sommet supérieur gauche
+    int cote; // Longueur du côté du carré
+};
